50696b798869b412a7a59650ec370be16bd8e71d
shubh-2601s <10221shubham.s@gmail.com>
2025-06-24 23:46:28 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -11,6 +11,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:3000"],  # update with your frontend origin
+    https://smart-charge-x.vercel.app/
     allow_methods=["*"],
     allow_headers=["*"],
 )
